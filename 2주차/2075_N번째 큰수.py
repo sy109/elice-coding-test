530e452be21d1398df03491b23dbd25ede2c22c2
sy109 <s-yoon3@wiu.edu>
2021-10-29 01:40:39 +0900

    N번째 큰수 풀이 완료

--- 2주차/2075_N번째 큰수.py
+++ 2주차/2075_N번째 큰수.py
@@ -5,13 +5,18 @@ def input():
     return sys.stdin.readline().rstrip()
 
 N = int(input())
-ans = [[] for i in range(N)]
+ans = []
 
 for i in range(N):
     lis = list(map(int,input().split()))
-    for count in range(len(lis)):
-        hq.heappush(ans[count],-lis[count%N])
-
-
-
-    
+    if not ans:
+        for item in lis:
+            hq.heappush(ans,item)
+    else:
+        for item in lis:
+            if ans[0] < item:
+                hq.heappop(ans)
+                hq.heappush(ans,item)
+            else:
+                continue
+print(ans[0])
